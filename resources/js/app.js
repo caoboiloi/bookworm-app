ce173d3ad6d031e6aeb7a00a674f10a5854df3fa
Huynh Loi <caoboiloi@gmail.com>
2021-07-16 19:05:40 +0700

    View: add header, footer component, config router for frontend

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,29 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../css/app.scss';
+import { HashRouter as Router, Route } from "react-router-dom";
+
+import '../scss/app.scss';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Header from './components/header';
+import Footer from './components/footer';
 
 class App extends React.Component {
     render() {
         return (
-            <h1>Hello mn</h1>
-        //   <Router>
-        //     <div className="App">
-        //       <Navbar />
+        <Router>
+            <div className="App">
+                <Header />
 
-        //       <Route exact path="/">
+                <Route exact path="/">
+                    <h1>Home</h1>
+                </Route>
+                <Route exact path="/product">
+                    <h1>product</h1>
+                </Route>
+                <Route exact path="/about">
+                    <h1>about</h1>
+                </Route>
+                <Route exact path="/cart">
+                    <h1>cart</h1>
+                </Route>
 
-        //       </Route>
-        //       <Route exact path="/contact">
-
-        //       </Route>
-        //       <Route exact path="/hello">
-
-        //       </Route>
-        //       {/* <Route path="**" render={() => <h1>Route not found!</h1>}></Route> */}
-        //     </div>
-        //   </Router>
+                <Footer />
+                {/* <Route path="**" render={() => <h1>Route not found!</h1>}></Route> */}
+            </div>
+        </Router>
         );
-      }
+    }
 }
 
 ReactDOM.render(
---
+++ resources/js/components/header/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './style.scss';
+
+import {Navbar, Nav} from 'react-bootstrap';
+
+import mainLogo from '../../../assets/bookworm_icon.svg';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header className="my-0">
+                <Navbar collapseOnSelect expand="md" bg="light">
+                    <Navbar.Brand href="#home">
+                        <img src={mainLogo} width="32" height="32" className="d-inline-block align-top my-auto" />
+                        <b className="ml-2">BOOKWORM</b>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ml-auto">
+                            <Nav.Link eventKey={1} href="#/">
+                                Home
+                            </Nav.Link>
+                            <Nav.Link eventKey={2} href="#/product">
+                                Shop
+                            </Nav.Link>
+                            <Nav.Link eventKey={3} href="#/about">
+                                About
+                            </Nav.Link>
+                            <Nav.Link eventKey={4} href="#/cart">
+                                Cart (<span>0</span>)
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </header>
+        );
+    }
+}
+
+export default Header;
