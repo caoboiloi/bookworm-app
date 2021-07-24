3503d492352ad346cf387c20c1d8f2c375124b45
Huynh Loi <caoboiloi@gmail.com>
2021-07-24 23:54:51 +0700

    style: fixed cruser pointer when hover for card, dropdown,...

--- resources/js/reducers/reviewReducer.js
+++ resources/js/reducers/reviewReducer.js
@@ -1,11 +1,16 @@
 import {
-    RELOAD_REVIEW_LIST_BOOK } from "../const/index";
+    RELOAD_REVIEW_LIST_BOOK,
+    DEFAULT_RELOAD_REVIEW_LIST_BOOK } from "../const/index";
 
 const reviewReducer = (state = [], action) => {
     switch (action.type) {
         case RELOAD_REVIEW_LIST_BOOK:
             return {
                 isReload : true
+            }
+        case DEFAULT_RELOAD_REVIEW_LIST_BOOK:
+            return {
+                isReload : false
             }
         default:
             return {
